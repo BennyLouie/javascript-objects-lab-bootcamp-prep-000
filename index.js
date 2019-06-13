8a0d9d6ade776405cecfe3e2f6ca89507626f242
Benny Louie <bennylouie1412@gmail.com>
2019-06-13 21:28:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-var recipes = {'eggs': '3 cups'}
+var recipes = {'eggs': '3 cups'};
 
 function updateObjectWithKeyAndValue(recipes, ingredient, amount) {
-  return Object.assign({}, recipes, {[ingredient]: amount})
+  return Object.assign({}, recipes, {[ingredient]: amount});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, ingredient, amount) {
   recipes[ingredient] = amount
-  return recipes
+  return recipes;
 }
 
 function deleteFromObjectByKey(recipes, ingredient) {
   var newRecipes = Object.assign({}, recipes)
   delete newRecipes[ingredient]
-  return newRecipes
+  return newRecipes;
 }
 
+function destructivelyDeleteFromObjectByKey(recipes, ingredient) {
+  delete recipes[ingredient]
+  return recipes;
+}
