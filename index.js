ed0bc1b904f2d4c10feb40c474a721eb40f4750c
Benny Louie <bennylouie1412@gmail.com>
2019-06-13 21:02:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {'eggs': '3 cups'}
 
 function updateObjectWithKeyAndValue(recipes, ingredient, amount) {
-  newRecipe = Object.assign({}, ingredient, amount)
+  return Object.assign({}, ingredient, amount)
   return newRecipe
 }
 
